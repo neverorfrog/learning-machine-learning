8724917e0553b25811f8a8ad5c8882181de24941
neverorfrog <97flavio.maiorana@gmail.com>
2023-12-21 17:19:07 +0100

    random changes

--- hw2/main.py
+++ hw2/main.py
@@ -3,18 +3,19 @@ from trainer import *
 from models import *
 from datamodule import Dataset
 warnings.filterwarnings("ignore")
-from utils import *
+from plotting_utils import *
 from car_env import *
+from training_utils import *
 
 car_env = CarEnv()
 dataset = Dataset(load = True)
 dataset.summarize()
 
-model = Model(name="dec-21", num_classes=5, lr=0.000001)
-trainer = Trainer(max_epochs = 1)
+model = Model(name="new", num_classes = 5, score_function = accuracy, lr=0.001)
+trainer = Trainer(max_epochs = 50, batch_size = 16)
 trainer.fit(model, dataset, plot = False)
 
 # model.load()
-evaluate(model, dataset)
+model.evaluate(dataset)
 
 car_env.play(model)
